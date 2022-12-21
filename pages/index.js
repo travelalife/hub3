05dc71b6ebffe526bb953d461a7e401c8b12da77
liulei-FE <1062591116@qq.com>
2022-12-21 17:26:59 +0800

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+
+    </Layout>
+  );
+}
